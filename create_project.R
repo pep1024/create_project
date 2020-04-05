c7775ddf43f5d8ca3f8e82cd08a7aadbeb0cce1d
pep1024 <josepmporra@gmail.com>
2020-04-05 21:54:01 +0200

    credentials added

--- create_project.R
+++ create_project.R
@@ -8,5 +8,13 @@ create_project(path = "C:/Users/josep.PORTATIL-ISABEL/Documents/R/shiny_game", o
 use_git()
 
 # 3. create a remote repo in github
-use_git
+use_github(protocol = "ssh")
 
+git_credentials()
+
+my_cred <- git2r::cred_ssh_key(
+  publickey = "/Users/josep.PORTATIL-ISABEL/Documents/.ssh/public_git_pep",
+  privatekey = "/Users/josep.PORTATIL-ISABEL/Documents/.ssh/private_git_pep.ppk", 
+  passphrase = "juliajoel")
+
+use_git_credentials(my_cred)
