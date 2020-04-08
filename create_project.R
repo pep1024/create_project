fb0ec42839f4d43ae3dfd9777c1d13017889a19d
pep1024 <josepmporra@gmail.com>
2020-04-08 22:39:10 +0200

    open new Rstudio session

--- create_project.R
+++ create_project.R
@@ -2,7 +2,7 @@
 library(usethis)
 
 # 1. create folder and create a new Rproj there.
-create_project(path = "C:/Users/josep.PORTATIL-ISABEL/Documents/R/shiny_game", open = FALSE, rstudio = TRUE)
+create_project(path = "C:/Users/josep.PORTATIL-ISABEL/Documents/R/shiny_game", open = TRUE, rstudio = TRUE)
 
 # 2. Init a local git repo in that directory
 use_git()
